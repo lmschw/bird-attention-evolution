log model params for DE runs & fix bearings issue for input computation

simulator/pigeon_simulator_2.py
@@ -161,7 +161,7 @@ class PigeonSimulator:
         plt.pause(0.000001)
 
     def move_heads(self, agents, distances, angles, perception_strengths_conspecifics):
-        inputs = np.array([wo.get_input_value_for_weight_option(weight_option=option, agents=agents, distances=distances, angles=angles, perception_strengths=perception_strengths_conspecifics) for option in self.weight_options])
+        inputs = np.array([wo.get_input_value_for_weight_option(weight_option=option, bearings=agents[:,4], distances=distances, angles=angles, perception_strengths=perception_strengths_conspecifics) for option in self.weight_options])
         inputs = np.where(inputs == np.inf, wo.MAX_INPUT, inputs)
         new_head_angles = []
         for i in range(self.num_agents):

simulator/pigeon_simulator_ae.py
@@ -257,7 +257,7 @@ class PigeonSimulatorAe:
             distances, angles = self.compute_distances_and_angles()
             perception_strengths_conspecifics, min_agent = pstrength.compute_perception_strengths(angles, distances, self.bird_type)
 
-            inputs = np.array([wo.get_input_value_for_weight_option(weight_option=option, agents=agents, distances=distances, angles=angles, perception_strengths=perception_strengths_conspecifics) for option in self.weight_options])
+            inputs = np.array([wo.get_input_value_for_weight_option(weight_option=option, bearings=agents[:,3], distances=distances, angles=angles, perception_strengths=perception_strengths_conspecifics) for option in self.weight_options])
             inputs = np.where(inputs == np.inf, wo.MAX_INPUT, inputs)
             new_head_angles = []
             for i in range(self.num_agents):

simulator/weight_options.py
@@ -3,7 +3,7 @@ from simulator.enum_weight_options import WeightOptions
 
 MAX_INPUT = 1_000_000
 
-def get_input_value_for_weight_option(weight_option, agents, distances, angles, perception_strengths):
+def get_input_value_for_weight_option(weight_option, bearings, distances, angles, perception_strengths):
     closest_neighbour = np.argmin(distances, axis=1)
     match weight_option:
         case WeightOptions.CLOSEST_DISTANCES:
@@ -22,7 +22,7 @@ def get_input_value_for_weight_option(weight_option, agents, distances, angles, 
             num_visible_agents = np.count_nonzero(perception_strengths, axis=1)
             return num_visible_agents
         case WeightOptions.PREVIOUS_HEAD_ANGLES:
-            previous_head_angles = agents[:,4]
+            previous_head_angles = bearings
             return previous_head_angles
         case WeightOptions.AVG_PERCEPTION_STRENGTHS:
             average_perception_strengths = np.average(perception_strengths, axis=1)
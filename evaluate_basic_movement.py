12a76d72b9cc2b92b0fab4d677ce86aaa8008d68
lmschw <lillyschwarzenbach@gmail.com>
2025-02-14 14:04:27 +0100

    update evaluation scripts

--- evaluate_basic_movement.py
+++ evaluate_basic_movement.py
@@ -8,11 +8,11 @@ from animal_models.wolf import Wolf
 from animal_models.zebra import Zebra
 from animal_models.zebrafinch import Zebrafinch
 
-n = 7
-for animal_type in [Pigeon(), Hawk(), Zebrafinch(), Zebra(), Wolf(), Rabbit()]:
-    base_save_path = f"basic_{animal_type.name}_n={n}"
-    save_path_agents = f"log_agents_{base_save_path}.csv"
-    evaluator = EvaluatorBasicMovement(data_file_path=save_path_agents, 
-                                    base_save_path=base_save_path,
-                                    max_iters=500)
-    evaluator.evaluate_and_visualise()
+for n in [5, 10, 20]:
+    for animal_type in [Pigeon(), Hawk()]:
+        base_save_path = f"basic_{animal_type.name}_n={n}"
+        save_path_agents = f"log_agents_{base_save_path}.csv"
+        evaluator = EvaluatorBasicMovement(data_file_path=save_path_agents, 
+                                        base_save_path=base_save_path,
+                                        max_iters=500)
+        evaluator.evaluate_and_visualise()
--- evaluate_corridor.py
+++ evaluate_corridor.py
@@ -7,11 +7,13 @@ from animal_models.wolf import Wolf
 from animal_models.zebra import Zebra
 from animal_models.zebrafinch import Zebrafinch
 
-n = 7
-for animal_type in [Pigeon(), Hawk(), Zebrafinch(), Zebra(), Wolf(), Rabbit()]:
-    base_save_path = f"basic_{animal_type.name}_n={n}"
-    save_path_agents = f"log_agents_{base_save_path}.csv"
-    evaluator = EvaluatorCorridor(data_file_path=save_path_agents, 
-                                    base_save_path=base_save_path,
-                                    max_iters=500)
-    evaluator.evaluate_and_visualise()
+for n in [7, 5]:
+    for animal_type in [Pigeon(), Hawk()]:
+        base_save_path = f"corridor_{animal_type.name}_n={n}"
+        save_path_agents = f"log_agents_{base_save_path}.csv"
+        evaluator = EvaluatorCorridor(data_file_path=save_path_agents, 
+                                        base_save_path=base_save_path,
+                                        max_iters=500,
+                                        corridor_centers=[[125, 27.5], [125, 67.5]],
+                                        corridor_endpoints=[[150, 27.5], [150, 67.5]])
+        evaluator.evaluate_and_visualise()

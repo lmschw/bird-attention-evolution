9d304dcf6b40aef8bb0155e00596fa4e98e91fb3
lmschw <lillyschwarzenbach@gmail.com>
2025-03-25 11:22:35 +0100

    add Deer and Lion classes

---
+++ animal_models/lion.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+from animal_models.animal import Animal
+from animal_models.focus_area import FocusArea
+
+"""
+Class inheriting from Animal including the relevant data.
+"""
+
+
+NAME = "Lion"
+AVERAGE_SPEED = 22.2 # m/s
+AVERAGE_SPEED = 0.222 # m/s
+#AVERAGE_SPEED = 0.01
+MIN_SPEED = AVERAGE_SPEED /2
+MAX_SPEED = AVERAGE_SPEED + (AVERAGE_SPEED/2)
+SPEEDS = (MIN_SPEED, AVERAGE_SPEED, MAX_SPEED)
+
+WIDTH = 0.5 # meters # assumption
+LENGTH = 1.6 # meters
+HEIGHT = 1.2 # meters
+HEAD_RANGE_HALF = np.deg2rad(120) # WARNING: THIS IS AN ASSUMPTION
+MAX_TURN_ANGLE = np.deg2rad(180) # WARNING: THIS IS AN ASSUMPTION
+PREFERRED_DISTANCE_FRONT_BACK = [LENGTH + 0.1, 2 * LENGTH] # WARNING: THIS IS AN ASSUMPTION
+PREFERRED_DISTANCE_LEFT_RIGHT = [2* WIDTH + 2, 4 * WIDTH + 1]  # WARNING: THIS IS AN ASSUMPTION
+SENSING_RANGE = 4000
+
+FOVEAL_REGION_NAME = "foveal_region_right"
+FOVEAL_REGION_AZIMUTH_HORIZONTAL = np.deg2rad(50)
+FOVEAL_REGION_FIELD_HORIZONTAL_HALF = np.deg2rad(80)
+FOVEAL_REGION_AZIMUTH_VERTICAL = np.deg2rad(0)
+FOVEAL_REGION_FIELD_VERTICAL_HALF = np.deg2rad(2)
+FOVEAL_REGION_COMFORTABLE_DISTANCE = (0, np.inf)
+FOVEAL_REGION_FOVEAL_ANGLE = np.deg2rad(50)
+FOVEAL_REGION_RIGHT = FocusArea(name=FOVEAL_REGION_NAME, 
+                          azimuth_angle_position_horizontal=FOVEAL_REGION_AZIMUTH_HORIZONTAL,
+                          angle_fovea_horizontal=FOVEAL_REGION_FOVEAL_ANGLE,
+                          angle_field_horizontal=FOVEAL_REGION_FIELD_HORIZONTAL_HALF,
+                          azimuth_angle_position_vertical=FOVEAL_REGION_AZIMUTH_VERTICAL,
+                          angle_fovea_vertical=FOVEAL_REGION_FOVEAL_ANGLE,
+                          angle_field_vertical=FOVEAL_REGION_AZIMUTH_VERTICAL,
+                          comfortable_distance=FOVEAL_REGION_COMFORTABLE_DISTANCE
+                          )
+
+FOVEAL_REGION_NAME = "foveal_region_left"
+FOVEAL_REGION_AZIMUTH_HORIZONTAL = np.deg2rad(-50)
+FOVEAL_REGION_FIELD_HORIZONTAL_HALF = np.deg2rad(80)
+FOVEAL_REGION_AZIMUTH_VERTICAL = np.deg2rad(0)
+FOVEAL_REGION_FIELD_VERTICAL_HALF = np.deg2rad(2)
+FOVEAL_REGION_COMFORTABLE_DISTANCE = (0, np.inf)
+FOVEAL_REGION_FOVEAL_ANGLE = np.deg2rad(50)
+FOVEAL_REGION_LEFT = FocusArea(name=FOVEAL_REGION_NAME, 
+                          azimuth_angle_position_horizontal=FOVEAL_REGION_AZIMUTH_HORIZONTAL,
+                          angle_fovea_horizontal=FOVEAL_REGION_FOVEAL_ANGLE,
+                          angle_field_horizontal=FOVEAL_REGION_FIELD_HORIZONTAL_HALF,
+                          azimuth_angle_position_vertical=FOVEAL_REGION_AZIMUTH_VERTICAL,
+                          angle_fovea_vertical=FOVEAL_REGION_FOVEAL_ANGLE,
+                          angle_field_vertical=FOVEAL_REGION_AZIMUTH_VERTICAL,
+                          comfortable_distance=FOVEAL_REGION_COMFORTABLE_DISTANCE
+                          )
+
+FOCUS_AREAS = [FOVEAL_REGION_LEFT, FOVEAL_REGION_RIGHT]
+FOVEAL_ACUITY = 0.9
+FOVEA_DISCOUNT_FACTOR = 0.6
+
+class Lion(Animal):
+    def __init__(self):
+        super().__init__(name=NAME,
+                         speeds=SPEEDS,
+                         width=WIDTH, 
+                         length=LENGTH, 
+                         head_range_half=HEAD_RANGE_HALF,
+                         max_turn_angle=MAX_TURN_ANGLE,
+                         focus_areas=FOCUS_AREAS, 
+                         foveal_acuity=FOVEAL_ACUITY,
+                         fovea_discount_factor=FOVEA_DISCOUNT_FACTOR,
+                         preferred_distance_front_back=PREFERRED_DISTANCE_FRONT_BACK,
+                         preferred_distance_left_right=PREFERRED_DISTANCE_LEFT_RIGHT,
+                         sensing_range=SENSING_RANGE)

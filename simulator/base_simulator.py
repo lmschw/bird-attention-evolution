dbfa7635dcd9e0986f0f09fb13cb6d5e7c81b630
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2025-06-26 16:34:02 +0200

    draw centroid trajectory

--- simulator/base_simulator.py
+++ simulator/base_simulator.py
@@ -70,7 +70,7 @@ class BaseSimulator:
         pos_xs = self.start_position[0] + xx.ravel() + (rng.random(n_points_x * n_points_y) * spacing * 0.5) - spacing * 0.25
         pos_ys = self.start_position[1] + yy.ravel() + (rng.random(n_points_x * n_points_y) * spacing * 0.5) - spacing * 0.25
         pos_hs = (rng.random(n_points_x * n_points_x) * 2 * np.pi) - np.pi
-
+        #pos_hs = np.full(len(pos_xs), np.pi)
         indices = np.random.choice(range(len(pos_xs)), self.num_agents, replace=False)
         pos_xs = pos_xs[indices]
         pos_ys = pos_ys[indices]
@@ -98,8 +98,14 @@ class BaseSimulator:
                     color="white", width=0.005, scale=40)
         
         self.ax.set_facecolor((0, 0, 0))
+        # Draw Trajectory
+        if len(self.centroid_trajectory) > 1:
+            x_traj, y_traj = zip(*self.centroid_trajectory)
+            self.ax.plot(x_traj, y_traj, color="orange")
 
+        self.ax.set_facecolor((0, 0, 0))
         centroid_x, centroid_y = np.mean(self.curr_agents[:, 0]), np.mean(self.curr_agents[:, 1])
+        print(centroid_x, centroid_y)
         if self.follow:
             self.ax.set_xlim(centroid_x-10, centroid_x+10)
             self.ax.set_ylim(centroid_y-10, centroid_y+10)
